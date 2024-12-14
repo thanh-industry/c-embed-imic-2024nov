6bdadb74a76d1cbc7990b4325c205f531d799119
huypqa <huyanhh195@gmail.com>
2024-12-14 10:18:08 +0700

    update gpio_toggle

--- Core/My_Lib/gpio.h
+++ Core/My_Lib/gpio.h
@@ -11,6 +11,23 @@
 #define ALTER_FUNC 2U
 #define ANALOG 3U
 
+#define GPIO_0  0
+#define GPIO_1  1
+#define GPIO_2  2
+#define GPIO_3  3
+#define GPIO_4  4
+#define GPIO_5  5
+#define GPIO_6  6
+#define GPIO_7  7
+#define GPIO_8  8
+#define GPIO_9  9
+#define GPIO_10 10
+#define GPIO_11 11
+#define GPIO_12 12
+#define GPIO_13 13
+#define GPIO_14 14
+#define GPIO_15 15
+#define GPIO_NUMBER 16
 enum{
 	GPIO_OFF,
 	GPIO_ON
@@ -20,4 +37,6 @@ enum{
 void gpio_init(void);
 void gpio_write(uint32_t pin, uint32_t value);
 uint32_t gpio_read(uint32_t pin);
+void gpio_toggle(uint32_t pin);
+
 #endif /* _GPIO_H_ */
