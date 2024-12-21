84b028fea7fc898c1fafc02d073b40db583cb15c
Dao Chau Nghia <daochaunghia94@gmail.com>
2024-12-21 20:22:02 +0700

    change init sequence

--- Core/Src/bus_init.c
+++ Core/Src/bus_init.c
@@ -15,12 +15,14 @@ void busInit(void) {
 }
 void busRegisterSetup(void) {
 	// Enable clock in AHB1
-	registerBitSet(REG_RCC_AHB1ENR, (BIT_0 | BIT_3 | BIT_7)); // For using general purpose I/O function PORT A, D, H
+	registerBitSet(REG_RCC_AHB1ENR, (BIT_0 | BIT_3 | BIT_7)); 			// For using general purpose I/O function PORT A, D, H
 
 	// Enable clock in APB1
-	registerBitSet(REG_RCC_APB1ENR, (BIT_4 | BIT_5)); // For using basic timer 6, 7
+	registerBitSet(REG_RCC_APB1ENR, (BIT_4 | BIT_5)); 					// For using basic timer 6, 7
+
 
 	// Enable clock in APB2
-	registerBitSet(REG_RCC_APB2ENR, BIT_14); // For using system configuration function in PORT A
+	registerBitSet(REG_RCC_APB2ENR, BIT_14); 							// For using system configuration function in PORT A
+	registerBitSet(REG_RCC_APB2ENR, BIT_0); 							// For using advanced timer 1
 
 }
