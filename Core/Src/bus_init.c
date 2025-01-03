92299da3f7014357cc5a57bf8e40c99f3934a751
Dao Chau Nghia <daochaunghia94@gmail.com>
2025-01-04 01:50:38 +0700

    added the bus register set up for i2c1

--- Core/Src/bus_init.c
+++ Core/Src/bus_init.c
@@ -15,14 +15,15 @@ void busInit(void) {
 }
 void busRegisterSetup(void) {
 	// Enable clock in AHB1
-	registerBitSet(REG_RCC_AHB1ENR, (BIT_0 | BIT_2 | BIT_3 | BIT_7)); 	// For using general purpose I/O function PORT A, C, D, H
+	registerBitSet(REG_RCC_AHB1ENR, (BIT_0 | BIT_1 | BIT_2 | BIT_3 | BIT_7)); 	// For using general purpose I/O function PORT A, B, C, D, H
 
 	// Enable clock in APB1
 	registerBitSet(REG_RCC_APB1ENR, (BIT_4 | BIT_5)); 					// For using basic timer 6, 7
-
+	registerBitSet(REG_RCC_APB1ENR, BIT_21); 							// For using I2C1
 
 	// Enable clock in APB2
 	registerBitSet(REG_RCC_APB2ENR, BIT_14); 							// For using system configuration function in PORT A
 	registerBitSet(REG_RCC_APB2ENR, (BIT_0 | BIT_1)); 					// For using advanced timer 1 and timer 8
+
 
 }
