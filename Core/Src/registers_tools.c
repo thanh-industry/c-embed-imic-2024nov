1fc073d17620cf68341ef933aa654ed3bf51e09a
Dao Chau Nghia <daochaunghia94@gmail.com>
2025-01-04 01:48:49 +0700

    added function version for uint8_t params and uint8_t returns

--- Core/Src/registers_tools.c
+++ Core/Src/registers_tools.c
@@ -15,7 +15,17 @@ uint32_t registerRead(uint32_t *registerAddress) {
 	return *registerAddress;
 }
 
+uint8_t registerRead_uint8(uint8_t *registerAddress) {
+	if (registerAddress == NULL) return 0;
+	return *registerAddress;
+}
+
 void registerWrite(uint32_t *registerAddress, uint32_t value) {
+	if (registerAddress == NULL) return;
+	*registerAddress = value;
+}
+
+void registerWrite_uint8(uint8_t *registerAddress, uint8_t value) {
 	if (registerAddress == NULL) return;
 	*registerAddress = value;
 }
@@ -27,6 +37,13 @@ void registerBitSet(uint32_t *registerAddress, uint32_t mask) {
 	registerWrite(registerAddress, reg);
 }
 
+void registerBitSet_uint8(uint8_t *registerAddress, uint8_t mask) {
+	if (registerAddress == NULL) return;
+	uint8_t reg = registerRead_uint8(registerAddress);
+	reg |= mask;
+	registerWrite_uint8(registerAddress, reg);
+}
+
 void registerBitClear(uint32_t *registerAddress, uint32_t mask) {
 	if (registerAddress == NULL) return;
 	uint32_t reg = registerRead(registerAddress);
@@ -34,9 +51,22 @@ void registerBitClear(uint32_t *registerAddress, uint32_t mask) {
 	registerWrite(registerAddress, reg);
 }
 
+void registerBitClear_uint8(uint8_t *registerAddress, uint8_t mask) {
+	if (registerAddress == NULL) return;
+	uint8_t reg = registerRead_uint8(registerAddress);
+	reg &= ~mask;
+	registerWrite_uint8(registerAddress, reg);
+}
+
+
 bool registerBitCheck(uint32_t *registerAddress, uint32_t mask) {
 	if (registerAddress == NULL) return false;
 	if (*registerAddress & mask) return true;
 	else return false;
 }
 
+bool registerBitCheck_uint8(uint8_t *registerAddress, uint8_t mask) {
+	if (registerAddress == NULL) return false;
+	if (*registerAddress & mask) return true;
+	else return false;
+}
